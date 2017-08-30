1be632240bd7e22c6a7da3ece4a7ef55752d188e
Sei Aoyumi <aoyumi@gamil.com>
2017-08-30 22:10:18 +0900

    Update Version 1.4  Add display of output file logs to console.  Change minimum quality setting to zero.  Add executable file check.  Fix comment lines.  Code refactoring.  update `README.md`

--- JPEG_Encode.bat
+++ JPEG_Encode.bat
@@ -1,30 +1,38 @@
 @echo off
-REM これはJPEGファイルをエンコードするパッチです。 
-REM フォルダ入力とファイル入力に対応しています。
-REM Version 1.31
-REM JPEGファイルの入力に対応（JPEGファイルを入力する場合はmozjpeg版cjpegが必須）
+REM JPEG encode batch for cjpeg - version 1.4
+REM   これはcjpegによるJPEGエンコードを簡単に扱えるようにするためのパッチです。 
+REM   複数のフォルダ入力とファイル入力に対応しています。
+REM   mozjpeg版cjpegを使用した場合は、JPEGファイルの再エンコードも可能です。
 
 setlocal enabledelayedexpansion
 
 REM ---- USER SETTING -------------------------------------
-REM cjpegの位置を絶対パスで指定
+REM "cjpeg.exe"の場所を絶対パスで指定（引用符有）
 set BIN_DIST=""
-REM cjpegに与えるオプションを設定
+
+REM "cjpeg.exe"に与えるオプションを設定（引用符無）
 REM example:
 REM   -baseline or -progressive (default)
 REM   -quality (0-100), -grayscale (Y only), -sample (2x2, 2x1, 1x1)
 set BIN_OPTION=
-REM エンコード品質をセット（1～100） 両方共0で無効
+
+REM 連続エンコード用の設定
+REM エンコード品質の最低値と最大値をセット（0～100の数値）
+REM   QUALITY_LO(LOW)…最低値、QUALITY_HI(HIGH)…最大値
+REM   両方同じ数字を指定すると無効
 set QUALITY_LO=0
-set QUALITY_UP=0
-REM 出力場所を指定
+set QUALITY_HI=0
+
+REM 出力パスを指定（引用符有）
 set OUT_DIR=""
+
 REM テストモード（1=有効、0=無効）
 set TEST_SW=0
-REM テストモード有効時の追加文字列
+REM テストモード有効時の追加文字列（引用符無）
 set TEST_NAME=mozjpeg
 
 REM --- BATCH SETTING -------------------------------------
+set OUT_EXT=.jpg
 set FLAG=FALSE
 set IFILE=0
 set OPFILE=0
@@ -33,31 +41,29 @@ set QA_MODE=0
 
 if %TEST_SW% == 1 (
   set TEST_NAME=" [%TEST_NAME% %BIN_OPTION%]"
+  echo %TEST_NAME%
 ) else (
   set TEST_NAME=""
 )
 
-echo %TEST_NAME%
-
-REM 数値じゃないデータが入っていた場合、0が入る処理
-set /a QUALITY_UP=%QUALITY_UP%
+REM 数値ではないデータが入っていた場合、0が入る
+set /a QUALITY_HI=%QUALITY_HI%
 set /a QUALITY_LO=%QUALITY_LO%
 
-REM 数値の範囲は1から100　条件を満たしていればモード(1)に変更
-if %QUALITY_UP% LSS 0 goto QA_CHECK_ERROR
+REM 数値の範囲は0から100　条件を満たしていればQA_MODE(1)に変更
+if %QUALITY_HI% LSS 0 goto QA_CHECK_ERROR
 if %QUALITY_LO% LSS 0 goto QA_CHECK_ERROR
-if %QUALITY_UP% GTR 100 goto QA_CHECK_ERROR
+if %QUALITY_HI% GTR 100 goto QA_CHECK_ERROR
 if %QUALITY_LO% GTR 100 goto QA_CHECK_ERROR
-if %QUALITY_LO% == 0 (
-  if %QUALITY_UP% == 0 (
-    set QA_MODE=0
-  )
+if %QUALITY_LO% LSS %QUALITY_HI% (
+ echo Encode Quality %QUALITY_LO% - %QUALITY_HI%
+ set QA_MODE=1
 ) else (
-  if %QUALITY_LO% LSS %QUALITY_UP% (
-   echo Encode Quality %QUALITY_LO% - %QUALITY_UP%
-   set QA_MODE=1
-  ) else goto QA_CHECK_ERROR
+ if %QUALITY_HI% NEQ %QUALITY_LO% goto QA_CHECK_ERROR
 )
+
+if not exist %BIN_DIST% goto EX_CHECK_ERROR
+
 echo. 
 mkdir %OUT_DIR% 2> nul
 for %%B in (%OUT_DIR%) do (
@@ -69,6 +75,11 @@ if not exist %OUT_DIR% (
 )
 
 goto MAINSTART
+
+:EX_CHECK_ERROR
+echo Detect error.
+echo   Illegal executable file path.
+goto FINAL
 
 :OD_CHECK_ERROR
 echo Detect error.
@@ -87,8 +98,8 @@ if not exist "%~1" goto AFTERWORKING
 if exist "%~1\" (
   call :GETCNUM "%~dp1"
   set NUM_OPLACE2= !GETCNUM_RET!
-  for /R "%~1" %%A in (*.bmp *.jpg) do (
-    echo ^ "%%~fA"
+  for /r "%~1" %%A in (*.bmp *.jpg) do (
+    echo In^|"%%~fA"
     set /a IFILE+=1
     call :RUNCUT "%%~dpnA" !NUM_OPLACE2!
     call :CONNECT_PATH %OUT_DIR% !WORKCUT!
@@ -96,65 +107,72 @@ if exist "%~1\" (
     mkdir !WORKFINDP! 2> nul
     set OUT_FILE=!WORKCONNECT!
     if %QA_MODE% == 1 (
-      for /L %%B in (%QUALITY_LO%, 1, %QUALITY_UP%) do (
+      for /l %%B in (%QUALITY_LO%, 1, %QUALITY_HI%) do (
         set /a OPFILE+=1
         call :CONNECT_PATH !OUT_FILE! "_[q%%B]"
         call :CONNECT_PATH !WORKCONNECT! %TEST_NAME%
-        call :CONNECT_PATH !WORKCONNECT! ".jpg"
+        call :CONNECT_PATH !WORKCONNECT! "%OUT_EXT%"
         call :DECIDENAME !WORKCONNECT!
         %BIN_DIST% %BIN_OPTION% -quality %%B -outfile !DN_FILENAME! "%%~fA"
-        call :CHECKFILE2 !DN_FILENAME!
-        if !ERRORLEVEL! GEQ 0 set /a OLFILE+=1
+        call :CHECKFILE !DN_FILENAME!
+        call :OUTWORK !ERRORLEVEL!
       )
     ) else (
       set /a OPFILE+=1
       call :CONNECT_PATH !OUT_FILE! %TEST_NAME%
-      call :CONNECT_PATH !WORKCONNECT! ".jpg"
+      call :CONNECT_PATH !WORKCONNECT! "%OUT_EXT%"
       call :DECIDENAME !WORKCONNECT!
       %BIN_DIST% %BIN_OPTION% -outfile !DN_FILENAME! "%%~fA"
-      call :CHECKFILE2 !DN_FILENAME!
-      if !ERRORLEVEL! GEQ 0 set /a OLFILE+=1
+      call :CHECKFILE !DN_FILENAME!
+      call :OUTWORK !ERRORLEVEL!
     )
   )
   goto NEXT
 )
 
 set FLAG=FALSE
-if "%~x1" == ".bmp" set FLAG=TRUE
-if "%~x1" == ".BMP" set FLAG=TRUE
-if "%~x1" == ".jpg" set FLAG=TRUE
-if "%~x1" == ".JPG" set FLAG=TRUE
+if /i "%~x1" == ".BMP" set FLAG=TRUE
+if /i "%~x1" == ".JPG" set FLAG=TRUE
 if %FLAG%==FALSE goto NEXT
 
-echo ^ "%~f1"
+echo In^|"%~f1"
 set /a IFILE+=1
 if %QA_MODE% == 1 (
-  for /L %%B in (%QUALITY_LO%, 1, %QUALITY_UP%) do (
+  for /l %%B in (%QUALITY_LO%, 1, %QUALITY_HI%) do (
     set /a OPFILE+=1
     call :CONNECT_PATH %OUT_DIR% "%~n1_[q%%B]"
     call :CONNECT_PATH !WORKCONNECT! %TEST_NAME%
-    call :CONNECT_PATH !WORKCONNECT! ".jpg"
+    call :CONNECT_PATH !WORKCONNECT! "%OUT_EXT%"
     call :DECIDENAME !WORKCONNECT!
     %BIN_DIST% %BIN_OPTION% -quality %%B -outfile !DN_FILENAME! "%~f1"
-    call :CHECKFILE2 !DN_FILENAME!
-    if !ERRORLEVEL! GEQ 0 set /a OLFILE+=1
+    call :CHECKFILE !DN_FILENAME!
+    call :OUTWORK !ERRORLEVEL!
   )
 ) else (
   set /a OPFILE+=1
   call :CONNECT_PATH %OUT_DIR% "%~n1"
   call :CONNECT_PATH !WORKCONNECT! %TEST_NAME%
-  call :CONNECT_PATH !WORKCONNECT! ".jpg"
+  call :CONNECT_PATH !WORKCONNECT! "%OUT_EXT%"
   call :DECIDENAME !WORKCONNECT!
   %BIN_DIST% %BIN_OPTION% -outfile !DN_FILENAME! "%~f1"
-  call :CHECKFILE2 !DN_FILENAME!
-  if !ERRORLEVEL! GEQ 0 set /a OLFILE+=1
+  call :CHECKFILE !DN_FILENAME!
+  call :OUTWORK !ERRORLEVEL!
 )
 
 :NEXT
 SHIFT
 GOTO MAINSTART
 
-REM --- SUB ROUTINE ---------------------------------------
+:OUTWORK
+if %1 GEQ 0 (
+  set /a OLFILE+=1
+  echo   ^|Out %DN_FILENAME%
+) else (
+  echo   ^|Err %DN_FILENAME%
+)
+exit /b
+
+REM --- COMMON SUB ROUTINE ---------------------------------------
 
 REM ---------------
 REM 重複するファイルがあった場合は自動的にファイル名に番号を追加する。
@@ -169,28 +187,19 @@ if %DN_NUM% == 0 (
   set DN_FILENAME="%~dpn1 (%DN_NUM%)%~x1"
 )
 call :CHECKFILE %DN_FILENAME%
-if %ERRORLEVEL% == 1 (
+if %ERRORLEVEL% GEQ 0 (
   set /a DN_NUM+=1
   goto :DECIDENAME_LOOP
 )
 exit /b
-:DECIDENAME_SET
 
 REM ---------------
-REM ファイルが存在しているかをチェックする。
-REM 引数はファイル名（フルパス）。
-REM 戻り値 0=存在していない 1=存在している
-:CHECKFILE
-if exist "%~f1" if not exist "%~f1\" exit /b 1
-exit /b 0
-
-REM ---------------
-REM ファイルの存在をとファイルサイズをチェックする。
-REM 戻り値 -1 ファイルが存在しない
+REM ファイルの存在とファイルサイズをチェックする。
+REM 戻り値 -1      ファイルが存在しない
 REM         0以上　ファイルが存在（ファイルサイズを返す）
-:CHECKFILE2
+:CHECKFILE
 if exist "%~f1\" exit /b -1
-if "%~z1" == "" exit /b -1
+if not exist "%~f1" exit /b -1
 exit /b %~z1
 
 REM ---------------
